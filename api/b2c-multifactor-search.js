33c5de36f956f85e4615022540e99b602ee4b041
lucasaribeiroufu-afk <lucas.aribeiro.ufu@gmail.com>
2026-09-16 12:34:41 -0300

    Update b2c-multifactor-search.js

--- api/b2c-multifactor-search.js
+++ api/b2c-multifactor-search.js
@@ -191,7 +191,7 @@ async function buscarFacebookGroups(query, location, nacional = false) {
       body: JSON.stringify({
         keywords: keywords,
         countryCode: 'br',
-        maxPosts: 30,               // 30 por keyword = ~600 max total
+       maxPosts: 15,               // 15 por keyword = ~330 max total 
         afterDate: 'last_month'
       })
     });
